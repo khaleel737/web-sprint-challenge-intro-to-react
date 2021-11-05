b894bac03807094be59ddea1c444365a1e142748
Khaleel Musleh <khaleelmusleh@Khaleels-MacBook-Pro.local>
2021-11-05 14:26:06 -0700

    i have no idea how i solved all of it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [people, setPeople] = useState([]);
+  // const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people').then( res => {
+      console.log(res.data)
+      setPeople(res.data);
+  }).catch( res => console.error(res.data));
+  }, [])
+
+  console.log(people);
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <>
+      <Character className="Character" data={people}/>
+      </>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,36 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components';
+
+const styledCard = styled.div`
+  opacity: 0;
+  transform: scale(2) rotateZ(180deg);
+  animation: 0.5s ease-in-out forwards;
+  background-color: purple;
+  h2 {
+    color: black;
+  }
+  p {
+    color: white;
+  }
+`
+
+
+export default function Character({data}) {
+    return (
+        <>
+            <h1>Character</h1>
+            <div columns={1}>
+            {data.map((character, i) => {
+                return (
+                    <styledCard className="Character" key={i}>
+                                {character.name}
+                    </styledCard>
+                );
+            })}
+            </div>
+        </>
+    );
+}
+
+
